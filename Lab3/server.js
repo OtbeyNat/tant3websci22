2b73c5e5f78b0590a310f1e198aacbd1ede2aa1b
Tobey Tan <tant3@rpi.edu>
2022-02-04 11:34:36 -0500

    front end done

---
+++ Lab3/server.js
@@ -0,0 +1,37 @@
+// server init + mods
+var app = require('express')();
+var http = require('http').Server(app);
+const { default: axios } = require('axios');
+const request = require('request');
+
+
+
+
+// server route handler
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/getByZip/:zipcode', (req, res) => {
+
+  const zip_code = req.params.zipcode;
+  // Make a request for a user with a given ID
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip_code}&units=imperial&appid=${apiKey}`;
+
+  axios.get(url)
+    .then(e => {
+      // console.log(e.data)
+      res.send(e.data)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+});
+
+
+
+// start server
+http.listen(3000, function(){
+  console.log('Server up on *:3000');
+});
